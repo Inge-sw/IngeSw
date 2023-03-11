32227fa2cb4921a3af7f4b494fd0d0d4c0799315
yuhang <51709956+yuhang23@users.noreply.github.com>
2023-03-11 12:23:44 +0100

    xml

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,14 @@
+import resto.Xml;
+import ristorante.Ricetta;
+
+import java.util.ArrayList;
+
 public class MainClass {
     public static void main(String[] args) {
+
+        ArrayList<Ricetta> Ricetta = new ArrayList<>();
+
         System.out.println("Hello World!");
+        Xml.leggiRicettario("Ricettario.xml", Ricetta);
     }
 }
