53b86cf2642abdf0afd071595b08bac239b99b4f
Olluz <e.marzullo@studenti.unibs.it>
2023-03-11 14:41:05 +0100

    fine oggi

--- src/ristorante/Ristorante.java
+++ src/ristorante/Ristorante.java
@@ -1,11 +1,16 @@
 package ristorante;
 
+import java.util.HashMap;
+
 public class Ristorante {
 
     private String nome;
     private int num_posti;
     private double carico_lavoro_persona;
     private double carico_lavoro_ristorante;
+    private HashMap<Bevanda, Double> bevande_persona;
+    private double qnt;
+    private HashMap<GeneriExtra, Double> generi_extra_persona;
 
     public Ristorante() {
 
@@ -15,5 +20,63 @@ public class Ristorante {
         this.nome = nome;
     }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNum_posti() {
+        return num_posti;
+    }
+
+    public void setNum_posti(int num_posti) {
+        this.num_posti = num_posti;
+    }
+
+    public double getCarico_lavoro_persona() {
+        return carico_lavoro_persona;
+    }
+
+    public void setCarico_lavoro_persona(double carico_lavoro_persona) {
+        this.carico_lavoro_persona = carico_lavoro_persona;
+    }
+
+    public double getCarico_lavoro_ristorante() {
+        return carico_lavoro_ristorante;
+    }
+
+    public void setCarico_lavoro_ristorante(double carico_lavoro_ristorante) {
+        this.carico_lavoro_ristorante = carico_lavoro_ristorante;
+    }
+
+    public HashMap<Bevanda, Double> getBevande_persona() {
+        return bevande_persona;
+    }
+
+    public void bevanda_persona() {
+        for (Bevanda bevanda : this.bevande_persona.keySet()){
+            this.bevande_persona.put(bevanda, qnt);
+        }
+    }
+
+    public HashMap<GeneriExtra, Double> getGeneri_extra_persona() {
+        return generi_extra_persona;
+    }
+
+    public double getQnt() {
+        return qnt;
+    }
+
+    public void setQnt(double qnt) {
+        this.qnt = qnt;
+    }
+
+    public void setGeneri_extra_persona() {
+        for (GeneriExtra genere : this.generi_extra_persona.keySet()){
+                this.generi_extra_persona.put(genere, qnt);
+        }
+    }
 }
