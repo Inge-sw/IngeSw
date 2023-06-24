b81bd7b3549aece119fad5a5a474fcdcba35e4dc
Olluz <e.marzullo@studenti.unibs.it>
2023-06-24 11:44:54 +0200

    pre e post condizioni di bevanda, generi, gestore, ingrediente\

--- src/ristorante/GeneriExtra.java
+++ src/ristorante/GeneriExtra.java
@@ -4,15 +4,29 @@ import resto.Misura;
 
 public class GeneriExtra extends Prodotto {
 
+    /*
+    * Precondizione: Deve essere passata una stringa "nome" non vuota come argomento.
+    * Postcondizione: Crea un nuovo oggetto di tipo "GeneriExtra" con il nome specificato
+    * e la misura predefinita "PEZZI" ereditata dalla classe "Prodotto".
+    * */
     public GeneriExtra(String nome) {
         super(nome, Misura.PEZZI);
     }
 
+    /*
+    * Precondizione: Nessuna.
+    * Postcondizione: Restituisce il codice hash calcolato dalla classe padre "Prodotto" per l'oggetto "GeneriExtra".
+    * */
     @Override
     public int hashCode() {
         return super.hashCode();
     }
 
+    /*
+    * Precondizione: Deve essere passato un oggetto "obj" come argomento.
+    * ostcondizione: Restituisce true se l'oggetto "obj" è uguale all'oggetto "GeneriExtra" corrente,
+    * utilizzando il metodo "equals" della classe padre "Prodotto". Restituisce false altrimenti.
+    * */
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
--- src/ristorante/Ingrediente.java
+++ src/ristorante/Ingrediente.java
@@ -6,15 +6,28 @@ public class Ingrediente extends Prodotto {
 
     private double quantita;
 
+    /*
+    * Precondizione: Devono essere passati una stringa "nome" non vuota e un valore double "quantita".
+    * Postcondizione: Crea un nuovo oggetto di tipo "Ingrediente" con il nome specificato, la misura predefinita "GRAMMI"
+    * ereditata dalla classe "Prodotto" e imposta la quantità dell'ingrediente con il valore specificato.
+    * */
     public Ingrediente(String nome, double quantita) {
         super(nome, Misura.GRAMMI);
         this.quantita = quantita;
     }
 
+    /*
+    * Precondizione: Nessuna.
+    * Postcondizione: Restituisce la quantità dell'ingrediente.
+    * */
     public double getQuantita() {
         return quantita;
     }
 
+    /*
+    * Precondizione: Nessuna.
+    * Postcondizione: Restituisce una stringa che rappresenta l'ingrediente.
+    * */
     public String toString() {
         return "\n\t- " + super.getNome() + " " + quantita + " " + Misura.GRAMMI;
     }
